b2fc24c64260028e1827d8799e653ebde207b9e5
shashi <shashi991988@gmail.com>
2015-04-13 23:52:16 +0530

    setting custom host and port

--- src/app.php
+++ src/app.php
@@ -5,17 +5,26 @@ use Silex\Provider\FormServiceProvider;
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+
+
 
 $app = new Application();
 
 $app->register(new FormServiceProvider());
+
+$app->register(new TranslationServiceProvider());
 
 $app->register(new MonologServiceProvider(), [
     'monolog.logfile' => __DIR__.'/dev.log',
 ]);
 
 $app->register(new UrlGeneratorServiceProvider());
+
+$app->register(new SessionServiceProvider());
+
 
 $app->register(new TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../web/views',
--- src/controller.php
+++ src/controller.php
@@ -1,12 +1,47 @@
 <?php
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\Request;
 use RedisBrow\Helper\RedisHelper;
 
-$app->get('/', function () use ($app) {
-    $client = new RedisHelper('127.0.0.1',6379);
+$app->get('/info', function () use ($app) {
+	$server = $app['session']->get('server');
+	$host = $server['host'];
+	$port = $server['port'];
+    $client = new RedisHelper($host,$port);
     $info = $client->info();
     $dbsize = $client->dbsize();
-    return $app['twig']->render('home.html.twig',array('info'=>$info,'dbsize'=>$dbsize));
+    return $app['twig']->render('info.html.twig',array('info'=>$info,'dbsize'=>$dbsize));
+
+})->bind('info');
+
+$app->match('/', function (Request $request) use ($app) {
+     // some default data for when the form is displayed the first time
+    $data = array(
+        'host' => '127.0.0.1',
+        'port' => '6379',
+    );
+
+    $form = $app['form.factory']->createBuilder('form', $data)
+        ->add('host')
+        ->add('port')
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+        $host = $data['host'];
+        $port = $data['port'];
+        $app['session']->set('server', array('host' => $host,'port'=>$port));
+
+       // var_dump($data);die();
+
+        return $app->redirect('/info');
+    }
+
+    // display the form
+    return $app['twig']->render('home.html.twig', array('form' => $form->createView()));
+
 
 })->bind('home');
